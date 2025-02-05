bec1b5511953cbff4cfe21b76d7fef2205f8d844
floriansajous@gmail.com <floriansajous@gmail.com>
2025-02-05 19:37:05 +0100

    added a CI workflow for github actions

--- my-first-app.Tests/obj/Debug/net9.0/my-first-app.Tests.AssemblyInfo.cs
+++ my-first-app.Tests/obj/Debug/net9.0/my-first-app.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-first-app.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2208a0954f79ecb3e2638dc9058f3cbbaf7102cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-first-app.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my-first-app.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- my-first-app/obj/Debug/net9.0/my-first-app.AssemblyInfo.cs
+++ my-first-app/obj/Debug/net9.0/my-first-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3339da42068a905e881f56aa563553e3c9b45dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2208a0954f79ecb3e2638dc9058f3cbbaf7102cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
